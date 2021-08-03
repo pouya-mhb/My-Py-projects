85445627ed14994fdcf744008bfd31cee1cbc405
pouya MHB <pouya.mehrbabak@gmail.com>
2021-08-03 17:33:53 +0430

    add excersie 5

---
+++ PublicProjects/1/tamrin5.py
@@ -0,0 +1,21 @@
+number = int(input("please enter a number : "))
+
+number = 5
+k = 2 * number - 2
+for i in range(0, number):
+    for j in range(0, k):
+        print(end=" ")
+    k = k - 1
+    for j in range(0, i + 1):
+        print("* ", end="")
+    print("")
+    
+k = number - 2
+
+for i in range(number, -1, -1):
+    for j in range(k, 0, -1):
+        print(end=" ")
+    k = k + 1
+    for j in range(0, i + 1):
+        print("* ", end="")
+    print("")
