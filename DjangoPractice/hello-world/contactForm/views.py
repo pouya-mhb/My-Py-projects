220e1813d2c526be53596096301e2898b72ab48a
Pouya Mehrbabak <pouya-mhb@users.noreply.github.com>
2021-08-22 21:37:12 +0430

    add query string

--- DjangoPractice/hello-world/contactForm/views.py
+++ DjangoPractice/hello-world/contactForm/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def register(request):
     # return render(request, 'index.html')
+    name = request.GET.get('name')
+    if name:
+        return HttpResponse(f"Hello, {name}.")
+    
     return HttpResponse("Hello, world.")
+
+    # localhost:8000/register?name=pouya
